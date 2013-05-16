1473727b34219a3266c4c7db1f1810c6c4382634
unknown <thithaole@web.de>
2013-05-16 13:59:01 +0200

    Basis Vektor

---
+++ GruppeB4/GruppeB4/GruppeB4/Vektor.h
@@ -0,0 +1,19 @@
+#ifndef VECTOR_H_
+#define VECTOR_H_
+
+//-----------------------------------------------------------------------------------------------------//
+//	Dies ist eine Struktur, die Positionen und Geschwindigkeiten im Spiel verwaltet.				   //																						   //
+//	Erstellt von Thao Le																			   //
+//	Die Datei Vektor.h darf nicht ohne Berechtigung kopiert oder verbreitet werden					   //																		   //
+//																									   //
+//																									   //
+//-----------------------------------------------------------------------------------------------------//
+struct s_Vector
+{
+
+int i_x;
+int i_y;
+
+
+};
+#endif
