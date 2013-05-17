ef48322e374c4f5f182282487468724287a24458
unknown <brandy92@hotmail.de>
2013-05-17 15:11:53 +0200

    Prinzessin ohne Bild

---
+++ GruppeB4/GruppeB4/Princess.cpp
@@ -0,0 +1,23 @@
+#include "Princess.h"
+
+
+
+
+
+
+
+
+
+
+
+void Princess::render(){
+
+
+
+	{
+	apply_Image(this->get_Position()->i_x,this->get_Position()->i_y,S_Resourcemanager::get_Resourcemanager()->get_Surface("Prinzessin"),S_Resourcemanager::get_Resourcemanager()->get_Surface("Screen"));
+}
+
+
+
+}
