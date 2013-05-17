4d7bb26c7645266de89495e5f8e552de32bb248a
Wladyk <w.malanichev@gmail.com>
2013-05-17 17:50:26 +0200

    level segment erweitert

--- GruppeB4/GruppeB4/LevelSegment.cpp
+++ GruppeB4/GruppeB4/LevelSegment.cpp
@@ -16,4 +16,40 @@ void LevelSegmente::init_Segmente()
 	SegmentRect12[0].y = 506;
 	SegmentRect12[0].w = 497;
 	SegmentRect12[0].h = 234;
+
+	SegmentRect12[1].x = 990;
+	SegmentRect12[1].y = 346;
+	SegmentRect12[1].w =  96;
+	SegmentRect12[1].h = 159;
+
+	SegmentRect13[0].x = 914;
+	SegmentRect13[0].y = 264;
+	SegmentRect13[0].w = 291;
+	SegmentRect13[0].h = 648;
+
+	SegmentRect13[1].x = 1098;
+	SegmentRect13[1].y = 173;
+	SegmentRect13[1].w = 106;
+	SegmentRect13[1].h = 90;
+
+	SegmentRect13[2].x = 710;
+	SegmentRect13[2].y = 83;
+	SegmentRect13[2].w = 495;
+	SegmentRect13[2].h = 199;
+
+	SegmentRect13[3].x = 710;
+	SegmentRect13[3].y = 176;
+	SegmentRect13[3].w = 124;
+	SegmentRect13[3].h = 106;
+
+	SegmentRect13[4].x = 574;
+	SegmentRect13[4].y = 177;
+	SegmentRect13[4].w = 134;
+	SegmentRect13[4].h = 77;
+
+	SegmentRect14[0].x = 78;
+	SegmentRect14[0].y = 78;
+	SegmentRect14[0].w = 496;
+	SegmentRect14[0].h = 266;
+
 }
