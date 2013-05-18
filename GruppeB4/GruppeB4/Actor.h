c6c215db934c33b1dbca65bd3abd336500a6cd8a
grombo93 <t.z.uckmantel@gmx.de>
2013-05-18 15:50:35 +0200

    neu

--- GruppeB4/GruppeB4/Actor.h
+++
@@ -1,9 +0,0 @@
-#ifndef ACTOR_H_
-#define ACTOR_H_
-
-class Actor
-{
-
-virtual void get_Position() = 0;
-virtual void set_Position() = 0;
-};#endif
--- GruppeB4/GruppeB4/globals.h
+++
@@ -1,8 +0,0 @@
-#ifndef GLOBALS_H_
-#define GLOBALS_H_
-
-
-const int  PLAYER_HEIGHT = 60;
-const int  PLAYER_WIDTH  = 60;
-
-#endif
