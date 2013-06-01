5af7ef11ed512f44e70e8e547071c6b8231414a9
grombo93 <t.z.uckmantel@gmx.de>
2013-06-01 18:02:30 +0200

    Scrolling jetzt richtig eingefuegt. Kollision mit Gegner wird jetzt im AgentManager gehandled.

---
+++ GruppeB4/GruppeB4/Agentmanager.cpp
@@ -0,0 +1,30 @@
+#include "AgentManager.h"
+
+void AgentManager::check_collision(Player * p_TempPlayer)
+{
+	 if(p_TempPlayer->get_Position()->i_x + PLAYER_WIDTH >= crazy_enemies[0]->get_Position()->i_x && p_TempPlayer->get_Position()->i_x <= crazy_enemies[0]->get_Position()->i_x && p_TempPlayer->get_Position()->i_y + PLAYER_HEIGHT >= crazy_enemies[0]->get_Position()->i_y && p_TempPlayer->get_Position()->i_y <= crazy_enemies[0]->get_Position()->i_y)//p_TempPlayer->get_Position()->i_y + PLAYER_HEIGHT >= crazy_enemies[0]->i_y && p_TempPlayer->get_Position()->i_x + PLAYER_WIDTH > crazy_enemies[0]->i_x && p_TempPlayer->get_Position()->i_x <= crazy_enemies[0]->i_x)
+		{
+			p_TempPlayer->set_Health(0);
+			
+		}
+	 else if(p_TempPlayer->get_Position()->i_x + PLAYER_WIDTH >= crazy_enemies[0]->get_Position()->i_x && p_TempPlayer->get_Position()->i_x <= crazy_enemies[0]->get_Position()->i_x && p_TempPlayer->get_Position()->i_y <= crazy_enemies[0]->get_Position()->i_y + PLAYER_HEIGHT && p_TempPlayer->get_Position()->i_y + PLAYER_HEIGHT >= crazy_enemies[0]->get_Position()->i_y + PLAYER_HEIGHT)
+		{
+		p_TempPlayer->set_Health(0);
+			
+		}
+	 else if(p_TempPlayer->get_Position()->i_x + PLAYER_WIDTH >= crazy_enemies[0]->get_Position()->i_x + PLAYER_WIDTH && p_TempPlayer->get_Position()->i_x <= crazy_enemies[0]->get_Position()->i_x + PLAYER_WIDTH && p_TempPlayer->get_Position()->i_y <= crazy_enemies[0]->get_Position()->i_y && p_TempPlayer->get_Position()->i_y + PLAYER_HEIGHT >= crazy_enemies[0]->get_Position()->i_y)
+		{
+			p_TempPlayer->set_Health(0);
+			
+			
+		
+			
+		}
+	 else if(p_TempPlayer->get_Position()->i_x + PLAYER_WIDTH >= crazy_enemies[0]->get_Position()->i_x + PLAYER_WIDTH && p_TempPlayer->get_Position()->i_x <= crazy_enemies[0]->get_Position()->i_x + PLAYER_WIDTH && p_TempPlayer->get_Position()->i_y <= crazy_enemies[0]->get_Position()->i_y + PLAYER_HEIGHT && p_TempPlayer->get_Position()->i_y + PLAYER_HEIGHT >= crazy_enemies[0]->get_Position()->i_y + PLAYER_HEIGHT)
+		{
+			p_TempPlayer->set_Health(0);
+			
+			
+			
+		}
+}
