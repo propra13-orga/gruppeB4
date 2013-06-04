182502a7e929b70d60dde610af0900e060a4fda8
kamui99 <w.malanichev@gmail.com>
2013-06-05 00:16:49 +0200

    Segmentierung fr lvl 1 neu

--- GruppeB4/GruppeB4/LevelSegment.cpp
+++ GruppeB4/GruppeB4/LevelSegment.cpp
@@ -2,54 +2,54 @@
 
 void LevelSegmente::init_Segmente()
 {
-	SegmentRect11[0].x = 219;
-	SegmentRect11[0].y = 551;
-	SegmentRect11[0].w = 280;
-	SegmentRect11[0].h = 173;
+	SegmentRect11[0].x = 394;
+	SegmentRect11[0].y = 633;
+	SegmentRect11[0].w = 532;
+	SegmentRect11[0].h = 476;
 
-	SegmentRect11[1].x = 500;
-	SegmentRect11[1].y = 615;
-	SegmentRect11[1].w = 211;
-	SegmentRect11[1].h = 87;
+	SegmentRect11[1].x = 927;
+	SegmentRect11[1].y = 772;
+	SegmentRect11[1].w =  40;
+	SegmentRect11[1].h = 209;
 
-	SegmentRect12[0].x = 710;
-	SegmentRect12[0].y = 506;
-	SegmentRect12[0].w = 497;
-	SegmentRect12[0].h = 234;
+	SegmentRect12[0].x = 968;
+	SegmentRect12[0].y = 633;
+	SegmentRect12[0].w = 866;
+	SegmentRect12[0].h = 476;
 
-	SegmentRect12[1].x = 990;
-	SegmentRect12[1].y = 346;
-	SegmentRect12[1].w =  96;
-	SegmentRect12[1].h = 159;
+	SegmentRect12[1].x = 1336;
+	SegmentRect12[1].y = 592;
+	SegmentRect12[1].w = 210;
+	SegmentRect12[1].h =  41;
 
-	SegmentRect13[0].x = 914;
-	SegmentRect13[0].y = 264;
-	SegmentRect13[0].w = 291;
-	SegmentRect13[0].h = 648;
+	SegmentRect13[0].x = 1235;
+	SegmentRect13[0].y = 371;
+	SegmentRect13[0].w = 599;
+	SegmentRect13[0].h = 221;
 
-	SegmentRect13[1].x = 1098;
-	SegmentRect13[1].y = 173;
-	SegmentRect13[1].w = 106;
-	SegmentRect13[1].h = 90;
+	SegmentRect13[1].x = 1677;
+	SegmentRect13[1].y =  97;
+	SegmentRect13[1].w = 157;
+	SegmentRect13[1].h = 273;
 
-	SegmentRect13[2].x = 835;
-	SegmentRect13[2].y = 83;
-	SegmentRect13[2].w = 265;
-	SegmentRect13[2].h = 90;
+	SegmentRect13[2].x = 1203;
+	SegmentRect13[2].y =  97;
+	SegmentRect13[2].w = 473;
+	SegmentRect13[2].h = 131;
 
-	SegmentRect13[3].x = 710;
-	SegmentRect13[3].y = 83;
-	SegmentRect13[3].w = 124;
-	SegmentRect13[3].h = 106;
+	SegmentRect13[3].x = 968;
+	SegmentRect13[3].y =  97;
+	SegmentRect13[3].w = 235;
+	SegmentRect13[3].h = 392;
 
-	SegmentRect13[4].x = 574;
-	SegmentRect13[4].y = 177;
-	SegmentRect13[4].w = 134;
-	SegmentRect13[4].h = 77;
+	SegmentRect13[4].x = 926;
+	SegmentRect13[4].y = 225;
+	SegmentRect13[4].w =  40;
+	SegmentRect13[4].h = 210;
 
-	SegmentRect14[0].x = 78;
-	SegmentRect14[0].y = 78;
-	SegmentRect14[0].w = 496;
-	SegmentRect14[0].h = 266;
+	SegmentRect14[0].x = 102;
+	SegmentRect14[0].y =  97;
+	SegmentRect14[0].w = 825;
+	SegmentRect14[0].h = 495;
 
 }
