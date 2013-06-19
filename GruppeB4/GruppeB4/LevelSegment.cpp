228d64a8e4ccaea91fe19ea9ff7bd116c1f7d00f
Wladyk Malanichev <w.malanichev@gmail.com>
2013-06-19 17:55:52 +0200

    lvl Segmentierung

--- GruppeB4/GruppeB4/LevelSegment.cpp
+++ GruppeB4/GruppeB4/LevelSegment.cpp
@@ -51,5 +51,69 @@ void LevelSegmente::init_Segmente()
 	SegmentRect14[0].y =  97;
 	SegmentRect14[0].w = 825;
 	SegmentRect14[0].h = 495;
+	
+	SegmentRect21[0].x =   7;
+	SegmentRect21[0].y = 750;
+	SegmentRect21[0].w =1955;
+	SegmentRect21[0].h = 740;
+	
+	SegmentRect21[1].x = 325;
+	SegmentRect21[1].y = 705;
+	SegmentRect21[1].w = 210;
+	SegmentRect21[1].h =  45;
 
+	SegmentRect22[0].x = 100;
+	SegmentRect22[0].y = 230;
+	SegmentRect22[0].w =1899;
+	SegmentRect22[0].h = 475;
+
+	SegmentRect31[0].x =   1;
+	SegmentRect31[0].y =  30;
+	SegmentRect31[0].w = 764;
+	SegmentRect31[0].h = 495;
+
+	SegmentRect31[1].x = 765;
+	SegmentRect31[1].y = 180;
+	SegmentRect31[1].w =  25;
+	SegmentRect31[1].h = 210;
+
+	SegmentRect32[0].x = 790;
+	SegmentRect32[0].y =  40;
+	SegmentRect32[0].w = 360;
+	SegmentRect32[0].h = 480; 
+
+	SegmentRect32[1].x = 966;
+	SegmentRect32[1].y = 520;
+	SegmentRect32[1].w =  56;
+	SegmentRect32[1].h =  30;
+
+	SegmentRect33[0].x = 870;
+	SegmentRect33[0].y = 550;
+	SegmentRect33[0].w = 235;
+	SegmentRect33[0].h = 390;
+
+	SegmentRect33[1].x = 190;
+	SegmentRect33[1].y = 710;
+	SegmentRect33[1].w = 680;
+	SegmentRect33[1].h = 145;
+
+	SegmentRect33[2].x = 190;
+	SegmentRect33[2].y = 855;
+	SegmentRect33[2].w = 250;
+	SegmentRect33[2].h = 175;
+
+	SegmentRect33[3].x = 190;
+	SegmentRect33[3].y =1030;
+	SegmentRect33[3].w =1755;
+	SegmentRect33[3].h = 150;
+
+	SegmentRect33[4].x =1515;
+	SegmentRect33[4].y = 711;
+	SegmentRect33[4].w = 151;
+	SegmentRect33[4].h = 319;
+
+	SegmentRect33[5].x =1205;
+	SegmentRect33[5].y =  60;
+	SegmentRect33[5].w = 765;
+	SegmentRect33[5].h = 651;
 }
--- GruppeB4/GruppeB4/LevelSegment.h
+++ GruppeB4/GruppeB4/LevelSegment.h
@@ -10,6 +10,11 @@ public:
 	SDL_Rect SegmentRect12[2];
 	SDL_Rect SegmentRect13[5];
 	SDL_Rect SegmentRect14[1];
+	SDL_Rect SegmentRect21[2];
+	SDL_Rect SegmentRect22[1];
+	SDL_Rect SegmentRect31[2];
+	SDL_Rect SegmentRect32[2];
+	SDL_Rect SegmentRect33[6];
 	void init_Segmente();
 };
 
