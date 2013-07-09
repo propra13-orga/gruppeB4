57f87d07345d81360fe30db19cd5db83dc33e2ac
Jan Scholz <mr.rofl@gmx.net>
2013-07-09 14:25:44 +0200

    Kommentierung und Code-Bereinigung Teil-2
    Außerdem Weapon.cpp und Armor.cpp entfernt

--- GruppeB4/GruppeB4/Armor.h
+++ GruppeB4/GruppeB4/Armor.h
@@ -15,15 +15,13 @@ private:
 	ARMOR_TYPE CURRENT_ARMOR;		
 	s_Vector * p_s_Position;
 
-	int i_dmg;
 public:
 
 	Armor(int x, int y,ARMOR_TYPE ARMORTOSET){CURRENT_ARMOR = ARMORTOSET;p_s_Position = new s_Vector;p_s_Position->i_x = x;p_s_Position->i_y = y;}
-	
-	void set_Position(int x, int y){this->p_s_Position->i_x = x;this->p_s_Position->i_y = y;}
+	void set_Position(int x, int y){this->p_s_Position->i_x = x;this->p_s_Position->i_y = y;} ///Setzt Ruestungsposition
 	s_Vector * get_Position(){return this->p_s_Position;}
-	ARMOR_TYPE get_Armor(){return this->CURRENT_ARMOR;}
-	void set_Weapon(ARMOR_TYPE TEMPARMOR){this->CURRENT_ARMOR = TEMPARMOR;}
+	ARMOR_TYPE get_Armor(){return this->CURRENT_ARMOR;}///Gibt aktuellen Ruestungstyp wieder
+	void set_Armor(ARMOR_TYPE TEMPARMOR){this->CURRENT_ARMOR = TEMPARMOR;}///Setzt aktuelle Ruestung
 	
 
 	
